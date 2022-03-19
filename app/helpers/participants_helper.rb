7db8e180aba23e45753cfac437b519afaab3e4f9
Karikay Sharma <karikaysharma@Karikays-MacBook-Pro-2021-M1-Pro.local>
2022-03-19 23:05:00 +0530

    moved particpant name method to helper

--- app/helpers/participants_helper.rb
+++ app/helpers/participants_helper.rb
@@ -1,2 +1,10 @@
 module ParticipantsHelper
+  def get_participant_names(participant_ids)
+    participants = Participant.find(participant_ids)
+    participants_name = []
+    participants.each { |participant|
+      participants_name.append(participant.name)
+    }
+    participants_name
+  end
 end
